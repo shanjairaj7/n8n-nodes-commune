99e90886883ee650af209cbe8e7cac007a5e7f88
Shanjai <shanjai@192.168.1.4>
2026-02-20 19:32:35 +0530

    chore: remove node_modules and dist from tracking, add .gitignore

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Commune } from './nodes/Commune/Commune.node';
-export { CommuneTrigger } from './nodes/CommuneTrigger/CommuneTrigger.node';
-export { CommuneApi } from './credentials/CommuneApi.credentials';
-//# sourceMappingURL=index.d.ts.map
